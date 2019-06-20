31272780f95e37d3c3eb7c3f0b59e7bbdd420e62
欧阳琪 <qi.ouyang@luckincoffee.com>
2019-06-20 16:47:14 +0800

    read

--- read.py
+++ read.py
@@ -6,7 +6,7 @@ import numpy as np
 I = Image.open('./image_1.png')
 I_array = np.array(I)
 print(I_array)
-# img = cv2.imread('D:/PyCharm Project/CNN/image_1.png')
+img = cv2.imread('D:/PyCharm Project/CNN/image_1.png')
 # cv2.imshow('image',img)
 # cv2.waitKey(0)
 # px = img[100,100]
