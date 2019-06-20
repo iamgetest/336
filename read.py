1be8bd7b8f805b4dcb20438ad4286959d4bf3dcd
欧阳琪 <qi.ouyang@luckincoffee.com>
2019-06-20 16:46:33 +0800

    first

---
+++ read.py
@@ -0,0 +1,13 @@
+import cv2
+from PIL import Image
+Image.MAX_IMAGE_PIXELS = None
+import numpy as np
+
+I = Image.open('./image_1.png')
+I_array = np.array(I)
+print(I_array)
+# img = cv2.imread('D:/PyCharm Project/CNN/image_1.png')
+# cv2.imshow('image',img)
+# cv2.waitKey(0)
+# px = img[100,100]
+# print(px)
