eea2b2a21061e2cf0675ba3baa666504ce19c1a0
欧阳琪 <qi.ouyang@luckincoffee.com>
2019-06-21 11:32:17 +0800

    1

--- read.py
+++ read.py
@@ -6,7 +6,15 @@ import numpy as np
 I = Image.open('./image_1.png')
 I_array = np.array(I)
 print(I_array)
-img = cv2.imread('D:/PyCharm Project/CNN/image_1.png')
+index = np.argwhere(I_array ==1)
+y = index[0:-1,0:1]
+uniques = np.unique(y)
+a=np.arange(28687)
+y_1 = uniques-a
+a_1 = np.unique(y_1)
+index_1 = np.argwhere(y_1 == 11062)
+print(index.shape)
+# img = cv2.imread('D:/PyCharm Project/CNN/image_1.png')
 # cv2.imshow('image',img)
 # cv2.waitKey(0)
 # px = img[100,100]
