fe79801854f0162a598f231565a7f6c374aea888
欧阳琪 <qi.ouyang@luckincoffee.com>
2019-06-21 15:51:40 +0800

    2

--- read.py
+++ read.py
@@ -2,20 +2,20 @@ import cv2
 from PIL import Image
 Image.MAX_IMAGE_PIXELS = None
 import numpy as np
+import matplotlib.pyplot as plt
 
-I = Image.open('./image_1.png')
-I_array = np.array(I)
-print(I_array)
-index = np.argwhere(I_array ==1)
-y = index[0:-1,0:1]
-uniques = np.unique(y)
-a=np.arange(28687)
-y_1 = uniques-a
-a_1 = np.unique(y_1)
-index_1 = np.argwhere(y_1 == 11062)
-print(index.shape)
-# img = cv2.imread('D:/PyCharm Project/CNN/image_1.png')
-# cv2.imshow('image',img)
-# cv2.waitKey(0)
-# px = img[100,100]
-# print(px)
+I = Image.open('D:/PyCharm Project/CNN/2.png')
+# I_array = np.array(I)
+# index = np.argwhere(I_array ==2)
+# y = index[0:-1,0:1]
+# uniques = np.unique(y)
+# a=np.arange(28687)
+# y_1 = uniques-a
+# a_1 = np.unique(y_1)
+# index_1 = np.argwhere(y_1 == 21014)
+
+
+
+box = (0,3694,2337,3959)
+roi = I.crop(box)
+roi.save('d:/02.png')
